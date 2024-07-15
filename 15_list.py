67859349a825bebb260543af91ebb4467d7a2a96
Priyanka mourya <priyankamourya030@gmail.com>
2024-07-15 18:48:31 +0530

    added practice code  15 to 17

---
+++ 15_list.py
@@ -0,0 +1,9 @@
+friends = ["sohan", "grapes", 4, 45.67, "rohan"]
+
+print(friends[0])
+
+friends[0] = "Orange" #unlike string list are mutable
+
+print(friends[4])
+
+print(friends[1:3])
---
+++ 16_listMethods.py
@@ -0,0 +1,20 @@
+friends = ["sohan", "grapes", 4, 45.67, "rohan"]
+
+print(friends)
+
+friends.append("Priyanka")
+
+print(friends)
+
+list1 = [ 3, 5, 54, 8, 12]
+
+#list1.sort()
+#list1.reverse()
+#list1.insert(2, 45) # Insert 45 such that its index in the list is 2
+
+value = list1.pop(3)
+print(value)
+#list1.pop(3)
+print(list1) 
+
+
